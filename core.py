58234c857bd46fc70c9296a957c354e9919d5406
Matt Behrens <askedrelic@gmail.com>
2012-07-10 23:22:13 -0700

    a few formatting changes and code re-org to make testing easier

--- core.py
+++ core.py
@@ -29,7 +29,9 @@ try:
 except KeyError, e:
     raise SystemExit('missing environment setting: %s' % e)
 
-# hack for Heroku's terrible scheduler API: 
+testing = os.environ.get('TESTING')
+
+# hack for Heroku's terrible scheduler API:
 # only run this script on specific days of the week
 # eg; 1,3 or 1 3
 dow_list = os.environ.get('DOW_LIST')
@@ -37,6 +39,11 @@ if dow_list:
     today = str(datetime.datetime.now().isoweekday())
     if today not in dow_list:
         raise SystemExit('today is not in dow list')
+
+output = {
+    'image_src': '',
+    'text': '',
+}
 
 br = mechanize.Browser()
 cj = cookielib.LWPCookieJar()
@@ -51,21 +58,15 @@ d = pq(resp.read())
 
 image = d('#tabs_vnd-1 img')
 if image:
-    image_src = "http://www.cater2.me" + image.attr.src
-    payload = '{"message":{"body":"%s"}}' %  image_src
-    url = 'https://%s.campfirenow.com/room/%s/speak.json' % (campfire_domain, room_id)
-    headers = {'content-type': 'application/json'}
-    auth = HTTPBasicAuth(campfire_auth, 'X')
-    try:
-        requests.post(url, data=payload, headers=headers, auth=auth)
-    except Exception, e:
-        print e
+    output['image_src'] = image.attr.src
 
-content = ''
+output['text'] += "Today's Lunch:"
+output['text'] += campfire_newline
+
 text = d('#tabs_vnd-1')
 original_text = text.text()
 prefix = re.sub('Menu.*', '', original_text)
-content += prefix
+output['text'] += prefix
 #try to remove outer html tags?
 body = pq(text.children('div').html())
 
@@ -77,21 +78,39 @@ good_body = body.replace('<u/>','').replace(u'<br/>','').replace('<b>','').repla
 good_body = re.sub('<span.*?>','',good_body)
 good_body = re.sub('Items have been.*\n*','',good_body, re.DOTALL|re.MULTILINE)
 
-content += campfire_newline
-content += campfire_newline
-content += good_body
+#specific formatting:
+good_body = good_body.replace('Allergen Key:', campfire_newline + 'Allergen Key:')
+good_body = good_body.replace(' *Vegetarian', '*Vegetarian')
+
+output['text'] += campfire_newline
+output['text'] += campfire_newline
+output['text'] += good_body
 
 #fix unicode content
-content = content.encode('ascii', 'ignore')
-# print content
+output['text'] = output['text'].encode('ascii', 'ignore')
 
-payload = '<message><type>PasteMessage</type><body>%s</body></message>' % content
-url = 'https://%s.campfirenow.com/room/%s/speak.xml' % (campfire_domain, room_id)
-headers = {'content-type': 'application/xml'}
-auth = HTTPBasicAuth(campfire_auth, 'X')
-try:
-    requests.post(url, data=payload, headers=headers, auth=auth)
-except Exception, e:
-    print e
+if testing:
+    print output['image_src']
+    print output['text']
+else:
+    if output['image_src']:
+        image_src = "http://www.cater2.me" + output['image_src']
+        payload = '{"message":{"body":"%s"}}' %  image_src
+        url = 'https://%s.campfirenow.com/room/%s/speak.json' % (campfire_domain, room_id)
+        headers = {'content-type': 'application/json'}
+        auth = HTTPBasicAuth(campfire_auth, 'X')
+        try:
+            requests.post(url, data=payload, headers=headers, auth=auth)
+        except Exception, e:
+            print e
+
+    payload = '<message><type>PasteMessage</type><body>%s</body></message>' % output['text']
+    url = 'https://%s.campfirenow.com/room/%s/speak.xml' % (campfire_domain, room_id)
+    headers = {'content-type': 'application/xml'}
+    auth = HTTPBasicAuth(campfire_auth, 'X')
+    try:
+        requests.post(url, data=payload, headers=headers, auth=auth)
+    except Exception, e:
+        print e
 
 print 'done. happy eating!'
